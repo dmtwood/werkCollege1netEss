76945ade3c66fb7507fc5cccbe066b3bbc9e0510
dmtwood <dimitri.gevers@gmail.com>
2020-10-19 12:01:28 +0200

    test eigenmaak branch

---
+++ werkversie1/WerkCollege1DG/WC1.Logica/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WerkCollege1DG.WC1.Logica
+{
+    class Helper
+    {
+        /// <summary>
+        /// Wisselt twee integers die als reference ingelezen worden.
+        /// </summary>
+        /// <param name="a">Bevat initieel de eerste waarde, na wissel de initiële tweede waarde. </param>
+        /// <param name="b">Bevat initieel de tweede waarde, na wissel de initiële eerste waarde. </param>
+        public static void Wissel(ref int a, ref int b)
+        {
+            int ezelsbrug = a;
+            a = b;
+            b = ezelsbrug;
+        }
+
+
+
+    }
+}
---
+++ werkversie1/WerkCollege1DG/WC1DotNetEss/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using WerkCollege1DG.WC1.Logica;
+
+namespace WerkCollege1DG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // lees twee integers in en wissel ze van plaats
+            Console.WriteLine(" ==== Helper.Wissel functie wisselt 2 waarden ");
+            int a = 0;
+            int b = 0;
+            Console.WriteLine("Geef een eerste waarde in");
+            string aS = Console.ReadLine();
+            a = int.Parse(aS);
+            Console.WriteLine("Geef een tweede waarde in");
+            string bS = Console.ReadLine();
+            b = int.Parse(bS);
+
+            Console.WriteLine("Waarden voor de wissel");
+            Console.WriteLine($"Eerste waarde = {a}.");
+            Console.WriteLine($"Tweede waarde = {b}.");
+
+            Helper.Wissel(ref a, ref b);
+
+            Console.WriteLine("Waarden na de wissel");
+            Console.WriteLine($"Eerste waarde = {a}.");
+            Console.WriteLine($"Tweede waarde = {b}.");
+
+
+
+
+        }
+    }
+}
